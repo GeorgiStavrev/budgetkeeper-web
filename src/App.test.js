fccb9c9173a3590d44559bc14384af33d1b424b8
GeorgiStavrev <georgi.stavrev@skyscanner.net>
2018-07-10 17:43:45 +0300

    fixes failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import App from './containers/App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { reducer } from './budgetkeeperRedux'
+import thunk from 'redux-thunk'
 it('renders without crashing', () => {
+  
+  const store = createStore(reducer, applyMiddleware(thunk))
+  
+  const AppWithStore = (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(AppWithStore, div);
   ReactDOM.unmountComponentAtNode(div);
 });
